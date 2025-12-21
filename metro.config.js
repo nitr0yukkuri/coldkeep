836693789f10c0da011a32fb4f6b31ff68193e77
wakato <itlogin0606@gmail.com>
2025-12-21 14:34:30 +0900

    やっと見えるようになった

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// tfliteをアセットとして追加
+// tfliteをアセットとして追加（これが無いと赤い画面のエラーになる）
 config.resolver.assetExts.push("tflite");
 
 module.exports = config;
